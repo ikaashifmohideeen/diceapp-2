ff0b46c7bbee16c6a282417990446105e916f616
ikaashifmohideen <kaashif.e0219035@sret.edu.in>
2021-01-12 16:27:50 +0530

    diceapp

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  int ldn=1;
+  int rdn=1;
+  chanceofdice(){
+    setState(() {
+      ldn= Random().nextInt(6) +1;
+      rdn= Random().nextInt(6) +1;
+    });
+  }
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                chanceofdice();
+              },
+              child: Image.asset("images/dice$ldn.png"),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                chanceofdice();
+              },
+              child: Image.asset("images/dice$rdn.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  }
+
+
